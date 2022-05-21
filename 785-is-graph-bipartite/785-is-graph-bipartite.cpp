2541a39ac79130594ca14e60657bad5b4cdd6e13
Prateek Praharaj <prateekpj873@gmail.com>
2022-05-21 09:38:57 +0530

    Time: 53 ms (12.58%), Space: 15.6 MB (8.59%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,34 +1,36 @@
 class Solution {
 public:
-    vector<int>vis,col;
-    bool dfs(int v, int c, vector<vector<int>>& graph){
-        vis[v]=1;
-        col[v]=c;
-        for(int child:graph[v]){
-            if(vis[child]==0){
-                // here c^1 is for flipping 1 by 0 or 0 by 1, that is flip the current color
-                if(dfs(child,c^1,graph)==false) 
-                    return false;
-            }
-            else{
-                if(col[v]==col[child])
-                    return false;
+    bool bpt(int src,vector<vector<int>> graph,int color[]){
+        color[src]=1;
+        queue<int> q;
+        q.push(src);
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            for(auto it: graph[node]){
+                if(color[it]==-1){
+                    color[it]=1-color[node];
+                    q.push(it);
+                }
+                else{
+                    if(color[node]==color[it]){
+                        return false;
+                    }
+                }
             }
         }
         return true;
     }
-    
     bool isBipartite(vector<vector<int>>& graph) {
-        int n=graph.size();
-        vis.resize(n);
-        col.resize(n);
-
-        for(int i=0;i<n;++i){
-            if(vis[i]==0 && dfs(i,0,graph)==false){ 
-                return false;
+        int color[graph.size()];
+        memset(color,-1,sizeof(color));
+        for(int i=0;i<graph.size();i++){
+            if(color[i]==-1){
+                if(!bpt(i,graph,color)){
+                    return false;
+                }
             }
         }
-        
         return true;
     }
 };
