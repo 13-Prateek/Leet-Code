a895855f538705c80fe37318a29a317e8ae82976
Prateek Praharaj <prateekpj873@gmail.com>
2022-05-25 08:04:50 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -4,51 +4,64 @@ using namespace std;
 
  // } Driver Code Ends
 
-
+struct node{
+    int u,v,wt;
+    node(int first, int second, int weight){
+        u=first;
+        v=second;
+        wt=weight;
+    }
+};
 class Solution
 {
 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
-    int spanningTree(int N, vector<vector<int>> adj[])
+	static bool MyComp(node a, node b){
+	    return a.wt<b.wt;
+	}
+	int findpar(int u, vector<int>& parent){
+	    if(u==parent[u]){
+	        return u;
+	    }
+	    return parent[u]=findpar(parent[u],parent);
+	}
+	void unionn(int u, int v, vector<int>&rank, vector<int>& parent){
+	    u=findpar(u,parent);
+	    v=findpar(v,parent);
+	    if(rank[u]<rank[v]){
+	        parent[u]=v;
+	    }
+	    else if(rank[v]<rank[u]){
+	        parent[v]=u;
+	    }
+	    else{
+	        parent[v]=u;
+	        rank[u]++;
+	    }
+	}
+    int spanningTree(int v, vector<vector<int>> adj[])
     {
         // code here
-         int parent[N+1]; 
-          
-        int key[N+1]; 
-          
-        bool mstSet[N+1]; 
-      
-        for (int i = 0; i <= N; i++) 
-            key[i] = INT_MAX, mstSet[i] = false; 
-        
-        priority_queue< pair<int,int>, vector <pair<int,int>> , greater<pair<int,int>> > pq;
-    
-        key[0] = 0; 
-        parent[0] = -1; 
-        pq.push({0, 0});
-        while(!pq.empty())
-        { 
-            int u = pq.top().second; 
-            pq.pop(); 
-            
-            mstSet[u] = true; 
-            
-            for (auto it : adj[u]) {
-                int v = it[0];
-                int weight = it[1];
-                if (mstSet[v] == false && weight < key[v]) {
-                    parent[v] = u;
-    		key[v] = weight; 
-                    pq.push({key[v], v});    
-                }
+        vector<int> parent(v);
+        vector<int> rank(v,0);
+        for(int i=0;i<v;i++){
+            parent[i]=i;
+        }
+        vector<node> edge;
+        for(int i=0;i<v;i++){
+            for(auto it: adj[i]){
+                edge.push_back(node(i,it[0],it[1]));
             }
-                
         }
-        int sum = 0;
-        for(int i = 1;i<=N;i++) {
-            if(key[i] != INT_MAX) sum += key[i];
+        sort(edge.begin(),edge.end(),MyComp);
+        int cost=0;
+        for(auto it: edge){
+            if(findpar(it.u,parent)!=findpar(it.v,parent)){
+                cost+=it.wt;
+                unionn(it.u,it.v,rank,parent);
+            }
         }
-        return sum; 
+        return cost;
     }
 };
 
