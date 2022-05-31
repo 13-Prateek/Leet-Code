ed31e97734c65b125f58d3632125700c21173b2a
Prateek Praharaj <prateekpj873@gmail.com>
2022-05-31 17:00:08 +0530

    Time: 33 ms (11.26%), Space: 10 MB (53.24%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.cpp
+++ 130-surrounded-regions/130-surrounded-regions.cpp
@@ -1,9 +1,7 @@
-
-int dx[]={-1,0,1,0};
-int dy[]={0,1,0,-1};
 class Solution {
 public:
-    
+    int dx[4]={-1,0,1,0};
+    int dy[4]={0,1,0,-1};
     void dfs(int x,int y, int row, int col,vector<vector<char>>& board){
         if(x<0 || y<0 || x>=row || y>=col || board[x][y]!='O'){
             return;
