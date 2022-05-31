d4310b779dbd33b1623705c3f66998880d36c669
Prateek Praharaj <prateekpj873@gmail.com>
2022-05-31 17:20:54 +0530

    Time: 62 ms (26.18%), Space: 13.3 MB (32.48%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,7 +1,8 @@
+int dx[4]={-1,0,1,0};
+int dy[4]={0,1,0,-1};
 class Solution {
 public:
-    int dx[4]={-1,0,1,0};
-    int dy[4]={0,1,0,-1};
+    
     void dfs(int x, int y, int row, int col, vector<vector<char>>& grid, vector<vector<int>>& vis){
         if(x<0 || y<0 || x>=row || y>=col || vis[x][y]==1 || grid[x][y]=='0'){
             return;
