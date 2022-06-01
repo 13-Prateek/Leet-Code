440c6da7667bed7a73c99c70705e9921d6cd6d24
Prateek Praharaj <prateekpj873@gmail.com>
2022-06-01 12:35:19 +0530

    Time: 15 ms (43.64%), Space: 14 MB (81.40%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-        int dx[4]={-1,0,1,0};
-        int dy[4]={0,1,0,-1};
-        int row=image.size();
-        int col=image[0].size();
-        int oc=image[sr][sc];
-        queue<pair<int,int>> q;
-        q.push({sr,sc});
-        vector<vector<int>> vis(row,vector<int> (col,0));
-        vis[sr][sc]=1;
-        while(!q.empty()){
-            auto p=q.front();
-            q.pop();
-            image[p.first][p.second]=newColor;
-            for(int i=0;i<4;i++){
-                int x=p.first+dx[i];
-                int y=p.second+dy[i];
-                if(x>=0 && y>=0 && x<row && y<col && vis[x][y]!=1 && image[x][y]==oc){
-                    q.push({x,y});
-                    vis[x][y]=1;
-                }
-            }
+   void dfs(vector<vector<int>>& image, int i, int j,int val, int newColor)
+    {
+        if(i<0 || i>=image.size() || j<0 || j>= image[0].size() || image[i][j] == newColor || image[i][j] != val)
+        {
+            return;
         }
+        image[i][j] = newColor;
+        dfs(image,i-1,j,val,newColor);
+        dfs(image,i+1,j,val,newColor);
+        dfs(image,i,j-1,val,newColor);
+        dfs(image,i,j+1,val,newColor);
+    }
+    
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor)
+    {
+        int val = image[sr][sc];
+        dfs(image,sr,sc,val,newColor);
         return image;
     }
 };
