01dd429ce37a5df96f89919ba19c6535f1fc14fa
Prateek Praharaj <prateekpj873@gmail.com>
2022-06-01 10:33:25 +0530

    Time: 35 ms (43.20%), Space: 13.7 MB (63.84%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
-    void dfs(int src, vector<int> adj[], vector<int>& vis){
-        vis[src]=1;
-        for(auto it: adj[src]){
-            if(!vis[it]){
-                dfs(it,adj,vis);
-            }
-        }
-    }
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        int n=isConnected.size();
-        vector<int> adj[n];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(i!=j && isConnected[i][j]==1){
-                    adj[i].push_back(j);
-                }
-            }
-        }
-        int count=0;
-        vector<int> vis(n,0);
-        for(int i=0;i<n;i++){
-            if(!vis[i]){
-                dfs(i,adj,vis);
-                count++;
-            }
-        }
-        return count;
-    }
+   int findCircleNum(vector<vector<int>>& M) {
+	int n=M.size(),ans=0;
+	if(n==0) return 0;
+
+	vector<bool>vis(n,false);
+
+	for(int i=0;i<n;i++)
+	{
+		if(!vis[i])
+		{
+			ans++;
+			dfs(M,vis,i);
+		}
+	}
+	return ans;
+}
+
+void dfs(vector<vector<int>>& M, vector<bool>& vis, int i)
+{
+	vis[i]=true;
+	for(int j=0;j<M.size();j++)
+		if(M[i][j]==1 && !vis[j])
+			dfs(M,vis,j);
+}
 };
