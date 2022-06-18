a960433c45613c48c4a5458e8f62d3d4dd5322e7
Prateek Praharaj <prateekpj873@gmail.com>
2022-06-18 09:46:17 +0530

    Time: 8 ms (66.00%), Space: 9.1 MB (7.74%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
+    int recur(int i, int j, vector<vector<int>>& triangle, vector<vector<int>>& dp){
+        if(i==triangle.size()-1){
+            return triangle[i][j];
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int down=triangle[i][j]+recur(i+1,j,triangle,dp);
+        int diag=triangle[i][j]+recur(i+1,j+1,triangle,dp);
+        return dp[i][j]=min(down,diag);
+    }
     int minimumTotal(vector<vector<int>>& triangle) {
-        int minSum = INT_MAX;
-          int h=triangle.size(),j;
-         j=h;
-        vector<vector<int>> v(h,vector<int> (j,0));
-         v[0][0]=triangle[0][0];
-         for(int i=1;i<h;i++)
-         {
-             for(int j=0;j<triangle[i].size();j++)
-             {
-                 if(j==0) v[i][j]=v[i-1][j]+triangle[i][j];
-                 else if(j==triangle[i].size()-1) v[i][j]=v[i-1][j-1]+triangle[i][j];
-                 else
-                 v[i][j]=min(v[i-1][j-1],v[i-1][j])+triangle[i][j];
-             }
-              
-         }
-         
-        return *min_element(v[h-1].begin(),v[h-1].end());
+        int n=triangle.size();
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+        for(int i=0;i<n;i++){
+            dp[i].resize(i+1);
+        }
+        return recur(0,0,triangle,dp);
     }
 };
