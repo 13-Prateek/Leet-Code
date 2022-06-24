b15372378181ff12c5680c4283a945740bcc8fc3
Prateek Praharaj <prateekpj873@gmail.com>
2022-06-24 20:48:30 +0530

    Time: 30 ms (24.77%), Space: 9.6 MB (6.09%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        if(x==0){
+            return true;
+        }
+        vector<int> v;
+        while(x>0){
+            v.push_back(x%10);
+            x=x/10;
+        }
+        int i=0,j=v.size()-1;
+        while(i<j){
+            if(v[i]!=v[j]){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
