89beb43c9ef53b9e4a6839212d6c55e7a69d7813
Prateek Praharaj <prateekpj873@gmail.com>
2022-06-29 12:15:42 +0530

    Time: 35 ms (48.45%), Space: 12.9 MB (52.71%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
-    int recur(int ind1, int ind2, string &s1, string &s2,vector<vector<int>> &dp){
-        if(ind1<0 || ind2<0){
-            return 0;
-        }
-        if(dp[ind1][ind2]!=-1){
-            return dp[ind1][ind2];
-        }
-        if(s1[ind1]==s2[ind2]){
-            return dp[ind1][ind2]=1+recur(ind1-1,ind2-1,s1,s2,dp);
-        }
-        return dp[ind1][ind2]=max(recur(ind1-1,ind2,s1,s2,dp),recur(ind1,ind2-1,s1,s2,dp));
-    }
+    // int recur(int ind1, int ind2, string &s1, string &s2,vector<vector<int>> &dp){
+    //     if(ind1==0 || ind2==0){
+    //         return 0;
+    //     }
+    //     if(dp[ind1][ind2]!=-1){
+    //         return dp[ind1][ind2];
+    //     }
+    //     if(s1[ind1-1]==s2[ind2-1]){
+    //         return dp[ind1][ind2]=1+recur(ind1-1,ind2-1,s1,s2,dp);
+    //     }
+    //     return dp[ind1][ind2]=max(recur(ind1-1,ind2,s1,s2,dp),recur(ind1,ind2-1,s1,s2,dp));
+    // }
     int longestCommonSubsequence(string s1, string s2) {
         int n=s1.length();
         int m=s2.length();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return recur(n-1,m-1,s1,s2,dp);
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        for(int ind1=1;ind1<=n;ind1++){
+        for(int ind2=1;ind2<=m;ind2++){
+            if(s1[ind1-1]==s2[ind2-1])
+                dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1];
+            else
+                dp[ind1][ind2] = 0 + max(dp[ind1-1][ind2],dp[ind1][ind2-1]);
+            }
+        }   
+        return dp[n][m];
     }
 };
