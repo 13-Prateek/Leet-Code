03a10a7afbdb275c4a4b479bed5192486920d237
Prateek Praharaj <prateekpj873@gmail.com>
2022-07-06 12:53:26 +0530

    Added solution - LeetHub

---
+++ Valid Expression - GFG/valid-expression.cpp
@@ -0,0 +1,35 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+bool valid(string str);
+int main()
+{
+    int t;
+    cin>>t;
+    cin.ignore();
+    while(t--)
+    {
+        string str;
+        getline(cin,str);
+        cout<<valid(str)<<endl;
+    }
+    return 0;
+}// } Driver Code Ends
+
+
+bool valid(string s)
+{
+    // code here
+    stack<char>st; 
+        for(auto it: s) {
+            if(it=='(' || it=='{' || it == '[') st.push(it); 
+            else {
+                if(st.size() == 0) return false; 
+                char ch = st.top(); 
+                st.pop(); 
+                if((it == ')' and ch == '(') or  (it == ']' and ch == '[') or (it == '}' and ch == '{')) continue;
+                else return false;
+            }
+        }
+        return st.empty(); 
+}
