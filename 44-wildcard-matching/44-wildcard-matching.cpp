7cf768c7d521116590d55f180e703a0c48b9a9f0
Prateek Praharaj <prateekpj873@gmail.com>
2022-07-10 21:37:36 +0530

    Time: 95 ms (48.49%), Space: 11.1 MB (68.80%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -1,37 +1,38 @@
 class Solution {
 public:
-    bool recur(int i, int j, string &s, string &p, vector<vector<int>> &dp){
-        if(i<0 && j<0){ //both strings exhausted
-            return true;
-        }
-        if(i>=0 && j<0){// 1st string exhaused but 2nd string still has characters
-            return false;
-        }
-        if(i<0 && j>=0){// 1st string still has some characters
-            for(int x=0;x<=j;x++){
-                if(p[x]!='*'){
-                    return false;
-                }
-            }
-            return true;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        //if characters match or s has ?
-        if(s[i]==p[j] || p[j]=='?'){
-            return dp[i][j]=recur(i-1,j-1,s,p,dp);
-        }
-        /*if we encounter an * */
-        if(p[j]=='*'){
-            return dp[i][j]=(recur(i,j-1,s,p,dp) || recur(i-1,j,s,p,dp));
-        }
-        return dp[i][j]=false;
+    bool isAllStars(string & S1, int i) {
+
+        // S1 is taken in 1-based indexing
+          for (int j = 1; j <= i; j++) {
+            if (S1[j - 1] != '*')
+              return false;
+          }
+          return true;
     }
     bool isMatch(string s, string p) {
         int n=s.length();
         int m=p.length();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return recur(n-1,m-1,s,p,dp);
+        vector<vector<bool>> dp(n+1,vector<bool>(m+1,0));
+        for(int i=0;i<=n;i++){
+            dp[i][0]=false;
+        }
+        for(int j=0;j<=m;j++){
+            dp[0][j]=isAllStars(p,j);
+        }
+        dp[0][0]=true;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+               if(s[i-1]==p[j-1] || p[j-1]=='?'){
+                     dp[i][j]=dp[i-1][j-1];
+                }
+                else if(p[j-1]=='*'){
+                     dp[i][j]=(dp[i][j-1] || dp[i-1][j]);
+                }
+                else{
+                    dp[i][j]=false;
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
