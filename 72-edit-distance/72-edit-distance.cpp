8610f89a6a6947923eb3dfac2c283a128bcd94d8
Prateek Praharaj <prateekpj873@gmail.com>
2022-07-10 18:53:55 +0530

    Time: 16 ms (67.54%), Space: 9 MB (56.89%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,26 +1,42 @@
 class Solution {
 public:
-    int recur(int i, int j, string &s1, string &s2,vector<vector<int>>& dp){
-        if(i<0){
-            return j+1;
-        }
-        if(j<0){
-            return i+1;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        if(s1[i]==s2[j]){
-            return dp[i][j]=0+recur(i-1,j-1,s1,s2,dp);
-        }
-        else{
-            return dp[i][j]=1+min(recur(i,j-1,s1,s2,dp),min(recur(i-1,j,s1,s2,dp),recur(i-1,j-1,s1,s2,dp)));
-        }
-    }
+    // int recur(int i, int j, string &s1, string &s2,vector<vector<int>>& dp){
+    //     if(i==0){
+    //         return j;
+    //     }
+    //     if(j==0){
+    //         return i;
+    //     }
+    //     if(dp[i][j]!=-1){
+    //         return dp[i][j];
+    //     }
+    //     if(s1[i-1]==s2[j-1]){
+    //         return dp[i][j]=0+recur(i-1,j-1,s1,s2,dp);
+    //     }
+    //     else{
+    //         return dp[i][j]=1+min(recur(i,j-1,s1,s2,dp),min(recur(i-1,j,s1,s2,dp),recur(i-1,j-1,s1,s2,dp)));
+    //     }
+    // }
     int minDistance(string s1, string s2) {
         int n=s1.length();
         int m=s2.length();
-        vector<vector<int>> dp(n,vector(m,-1));
-        return recur(n-1,m-1,s1,s2,dp);
+        vector<vector<int>> dp(n+1,vector(m+1,0));
+        for(int i=0;i<=m;i++){
+            dp[0][i]=i;
+        }
+        for(int i=0;i<=n;i++){
+            dp[i][0]=i;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1[i-1]==s2[j-1]){
+                    dp[i][j]=0+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=1+min(dp[i-1][j],min(dp[i][j-1],dp[i-1][j-1]));
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
