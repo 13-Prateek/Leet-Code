731bdb701ec06a1e020e4922a102f6cebaafe83f
Prateek Praharaj <prateekpj873@gmail.com>
2022-07-11 11:45:30 +0530

    Time: 540 ms (26.11%), Space: 11.5 MB (58.63%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int len = s.size();
-    int dp[len][len];
-    memset(dp,0,sizeof(dp));
-    int end=1;
-    int start=0;
-	
-    for(int i=0;i<len;i++)
-    {
-        dp[i][i] = 1;
-    }
-    for(int i=0;i<len-1;i++)
-    {
-        if(s[i]==s[i+1])
-        { dp[i][i+1]=1;start=i;end=2;}
-    }
-    
-    for(int j=2;j<len;j++)
-    {
-        for(int i=0;i< len-j;i++)
-        {  
-            int left=i; //start point
-            int right = i+j;  //ending point
-            
-            if(dp[left+1][right-1]==1 && s[left]==s[right]) 
-            {
-                dp[left][right]=1; start=i; end=j+1; 
-            }        
+        if(s.size() == 0) return "";
+        int n = s.size();
+        int table[n][n];
+        memset(table,-1,sizeof(table)); //Initializing table with -1
+        for (int i = 0; i < n; ++i) 
+            table[i][i] = 1;  //Filling up the table
+        int maxLen = 1;
+        int start = 0; 
+        for (int i = 0; i < n - 1; ++i) { //checking for substring of length 2.
+            if (s[i] == s[i + 1]) { 
+                table[i][i + 1] = 1; 
+                start = i;  //Updating the start
+                maxLen = 2; 
+            } 
         }
-    }
-   return s.substr(start, end);
+        for(int k=3;k<=n;k++){ //Checking for length greater than 2 and k is length of substring 
+            for(int i=0;i<n - k + 1;i++){
+                int j = i + k - 1; //Initializing the end Index i.e j = i + k - 1
+                if(table[i+1][j-1] == 1 && s[i] == s[j]){ //Checking for the conditions i.e checking for sub-string from ith index to jth index.
+                    table[i][j] = 1;
+                    if (k > maxLen) { //Updating the maxLen
+                        start = i;  //Updating the start
+                        maxLen = k; 
+                    } 
+                }
+            }
+        }
+        return s.substr(start, maxLen);
     }
 };
