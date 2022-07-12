af96ecd378aca94c22cf218e44e0da61fc2ae12a
Prateek Praharaj <prateekpj873@gmail.com>
2022-07-12 12:17:04 +0530

    Time: 10 ms (74.47%), Space: 7.2 MB (77.97%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,27 +1,15 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length()!=t.length()){
-            return false;
+        if (s.length() != t.length()) return false;
+        int n = s.length();
+        int counts[26] = {0};
+        for (int i = 0; i < n; i++) { 
+            counts[s[i] - 'a']++;
+            counts[t[i] - 'a']--;
         }
-        unordered_map<char,int> mp;
-        for(int i=0;i<s.length();i++){
-            mp[s[i]]++;
-        }
-        for(int i=0;i<t.length();i++){
-            if(mp.find(t[i])==mp.end()){
-                return false;
-            }
-            else{
-                mp[t[i]]--;
-                if(mp[t[i]]==0){
-                    mp.erase(t[i]);
-                }
-            }
-        }
-        if(mp.size()!=0){
-            return false;
-        }
+        for (int i = 0; i < 26; i++)
+            if (counts[i]) return false;
         return true;
     }
 };
