0792adbb666388004b9e9cb4deb72a336afbcab5
Prateek Praharaj <prateekpj873@gmail.com>
2022-07-18 11:18:01 +0530

    Time: 8 ms (51.59%), Space: 11.9 MB (45.03%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -18,7 +18,19 @@ public:
     }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<int>> dp(n+2,vector(2,-1));
-        return recur(0,1,n,prices,dp);
+        vector<vector<int>> dp(n+2,vector(2,0));
+        for(int ind=n-1;ind>=0;ind--){
+            for(int buy=0;buy<=1;buy++){
+                int profit;
+                if(buy==1){
+                    profit=max(-prices[ind]+dp[ind+1][0],0+dp[ind+1][1]);
+                }
+                else{
+                    profit=max(prices[ind]+dp[ind+2][1],0+dp[ind+1][0]);
+                }
+                dp[ind][buy]=profit;
+            }
+        }
+        return dp[0][1];
     }
 };
