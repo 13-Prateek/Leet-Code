1bfa9478f78c3d08b511275a89b2879d5c316687
Prateek Praharaj <prateekpj873@gmail.com>
2022-07-18 11:28:29 +0530

    Time: 157 ms (72.18%), Space: 55 MB (90.73%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,24 +1,37 @@
 class Solution {
 public:
-     int recur(int ind, int buy, int n,int fee, vector<int> &prices,vector<vector<int>> &dp){
-        if(ind==n){
-            return 0;
-        }
-        if(dp[ind][buy]!=-1){
-            return dp[ind][buy];
-        }
-        int profit=0;
-        if(buy==1){
-            profit=max(-prices[ind]+recur(ind+1,0,n,fee,prices,dp),0+recur(ind+1,1,n,fee,prices,dp));
-        }
-        else{
-            profit=max(prices[ind]-fee+recur(ind+1,1,n,fee,prices,dp),0+recur(ind+1,0,n,fee,prices,dp));
-        }
-        return dp[ind][buy]=profit;
-    }
+    //  int recur(int ind, int buy, int n,int fee, vector<int> &prices,vector<vector<int>> &dp){
+    //     if(ind==n){
+    //         return 0;
+    //     }
+    //     if(dp[ind][buy]!=-1){
+    //         return dp[ind][buy];
+    //     }
+    //     int profit=0;
+    //     if(buy==1){
+    //         profit=max(-prices[ind]+recur(ind+1,0,n,fee,prices,dp),0+recur(ind+1,1,n,fee,prices,dp));
+    //     }
+    //     else{
+    //         profit=max(prices[ind]-fee+recur(ind+1,1,n,fee,prices,dp),0+recur(ind+1,0,n,fee,prices,dp));
+    //     }
+    //     return dp[ind][buy]=profit;
+    // }
     int maxProfit(vector<int>& prices, int fee) {
         int n=prices.size();
-        vector<vector<int>> dp(n+1,vector(2,-1));
-        return recur(0,1,n,fee,prices,dp);
+        vector<int> ahead(2,0), cur(2,0);
+        for(int ind=n-1;ind>=0;ind--){
+            for(int buy=0;buy<=1;buy++){
+                int profit;
+                if(buy==1){
+                    profit=max(-prices[ind]+ahead[0],0+ahead[1]);
+                }
+                else{
+                    profit=max(prices[ind]-fee+ahead[1],0+ahead[0]);
+                }
+                cur[buy]=profit;
+            }
+            ahead=cur;
+        }
+        return ahead[1];
     }
 };
