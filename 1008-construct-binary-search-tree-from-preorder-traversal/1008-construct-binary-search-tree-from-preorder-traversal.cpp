395d1186670852feef0fc019ba423293c5e69aaa
Prateek Praharaj <prateekpj873@gmail.com>
2022-07-31 12:51:53 +0530

    Time: 7 ms (69.34%), Space: 14.1 MB (16.11%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -12,14 +12,14 @@
 class Solution {
 public:
     TreeNode* ctree(vector<int>& preorder, int preS, int preE, vector<int>& inorder, int inS, int inE, map<int,int>& m){
-        if(inS> inE || preS>preE){
+        if(preS>preE || inS>inE){
             return NULL;
         }
-        TreeNode* root= new TreeNode(preorder[preS]);
-        int elem=m[root->val];
+        TreeNode* root=new TreeNode(preorder[preS]);
+        int elem=m[preorder[preS]];
         int nelem=elem-inS;
         root->left=ctree(preorder,preS+1,preS+nelem,inorder,inS,elem-1,m);
-        root->right=ctree(preorder,preS+1+nelem,preE,inorder,elem+1,inE,m);
+        root->right=ctree(preorder,preS+nelem+1,preE,inorder,elem+1,inE,m);
         return root;
     }
     TreeNode* bstFromPreorder(vector<int>& preorder) {
@@ -29,12 +29,10 @@ public:
             inorder[i]=preorder[i];
         }
         sort(inorder.begin(),inorder.end());
-        int inS=0, inE=n-1;
-        int preS=0, preE=n-1;
         map<int,int> m;
         for(int i=0;i<n;i++){
             m[inorder[i]]=i;
         }
-        return ctree(preorder,preS,preE,inorder,inS,inE,m);
+        return ctree(preorder,0,n-1,inorder,0,n-1,m);
     }
 };
