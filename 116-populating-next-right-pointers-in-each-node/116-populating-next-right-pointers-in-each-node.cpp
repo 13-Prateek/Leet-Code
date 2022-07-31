b0480b18e0eaafd4ca4696037650f846a366e339
Prateek Praharaj <prateekpj873@gmail.com>
2022-07-31 11:18:07 +0530

    Time: 24 ms (78.60%), Space: 16.8 MB (87.25%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cpp
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cpp
@@ -19,26 +19,17 @@ public:
 class Solution {
 public:
     Node* connect(Node* root) {
-        if(!root){
-            return NULL;
-        }
-        queue<Node*> q;
-        q.push(root);
-        while(!q.empty()){
-            int n=q.size();
-            for(int i=0;i<n;i++){
-                auto node=q.front();
-                q.pop();
-                if(i!=n-1){
-                    node->next=q.front();
-                }
-                if(node->left){
-                    q.push(node->left);
-                }
-                if(node->right){
-                    q.push(node->right);
-                }
+        Node *prev = root, *curr;
+        while (prev) {
+            curr = prev;
+            while (curr && curr->left) { 
+                //connects the left subtree of same level with right subtree of that same level 
+                curr->left->next = curr->right;
+                //connect the rightmost node of a level to the leftmost node of the next level.
+                if (curr -> next) curr->right->next = curr->next->left;
+                curr = curr->next;
             }
+            prev = prev -> left;
         }
         return root;
     }
