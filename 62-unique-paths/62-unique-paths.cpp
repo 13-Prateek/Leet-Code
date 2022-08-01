7a7280f0b05f8a8fcf2f8a269adbdfb5efa65dd5
Prateek Praharaj <prateekpj873@gmail.com>
2022-08-01 06:25:57 +0530

    Time: 3 ms (44.81%), Space: 6.5 MB (26.54%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
+    int recur(int i, int j, int m, int n,vector<vector<int>> &dp){
+        if(i==m-1 && j==n-1){
+            return 1;
+        }
+        if(i>m-1 || j>n-1){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int right=recur(i,j+1,m,n,dp);
+        int down=recur(i+1,j,m,n,dp);
+        return dp[i][j]=right+down;
+    }
     int uniquePaths(int m, int n) {
-        int N = n + m - 2;
-            int r = m - 1; 
-            double res = 1;
-            
-            for (int i = 1; i <= r; i++)
-                res = res * (N - r + i) / i;
-            return (int)res;
+        vector<vector<int>> dp(m,vector(n,-1));
+        
+        return recur(0,0,m,n,dp);
     }
 };
