3f45936549152f2106dc4f81ddbf360f67bdf2f9
Prateek Praharaj <prateekpj873@gmail.com>
2022-08-04 08:01:49 +0530

    Time: 29 ms (71.02%), Space: 13.1 MB (92.01%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,36 +1,37 @@
 class Solution {
 public:
-    bool dfs(int src, vector<int> adj[], vector<int> &vis, vector<int> &dfsrec){
-        vis[src]=true;
-        dfsrec[src]=true;
-        //cycle exits if there a nde appears twice in recursion call stack
-        for(auto it:adj[src]){
-            if(!vis[it]){
-                if(dfs(it,adj,vis,dfsrec)){
-                    return true;
-                }
-            }
-            else if(dfsrec[it]){
-                return true;
-            }
-        }
-        dfsrec[src]=false;
-        return false;
-    }
-    bool canFinish(int numc, vector<vector<int>>& prereq) {
-        vector<int> adj[numc];
+    bool canFinish(int v, vector<vector<int>>& prereq) {
+        vector<int> adj[v];
         for(int i=0;i<prereq.size();i++){
             adj[prereq[i][1]].push_back(prereq[i][0]);
         }
-        vector<int> vis(numc,0);
-        vector<int> dfsrec(numc,0);
-        for(int i=0;i<numc;i++){
-            if(!vis[i]){
-                if(dfs(i,adj,vis,dfsrec)){
-                    return false;
-                }
-            }
+        vector<int> indegree(v,0);
+	    for(int i=0;i<v;i++){
+	        for(auto it:adj[i]){
+	            indegree[it]++;
+	        }
+	    }
+	    queue<int> q;
+	    for(int i=0;i<v;i++){
+	        if(indegree[i]==0){
+	            q.push(i);
+	        }
+	    }
+	    vector<int> ans;
+	    while(!q.empty()){
+	        int node=q.front();
+	        q.pop();
+	        ans.push_back(node);
+	        for(auto it:adj[node]){
+	            indegree[it]--;
+	            if(indegree[it]==0){
+	                q.push(it);
+	            }
+	        }
+	    }
+        if(ans.size()==v){
+            return true;
         }
-        return true;
+        return false;
     }
 };
