c9b482786a003dd9900702cfe9cf2d4edd0f6729
Prateek Praharaj <prateekpj873@gmail.com>
2022-08-06 23:35:40 +0530

    Time: 71 ms (27.16%), Space: 21.3 MB (13.09%) - LeetHub

---
+++ 1392-longest-happy-prefix/1392-longest-happy-prefix.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string longestPrefix(string s) {
+        int n=s.length();
+        int len=0;
+        string ans="";
+        vector<int> lps(n,0);
+        lps[0]=0;
+        int i=1;
+        while(i<n){
+            if(s[i]==s[len]){
+                len++;
+                lps[i]=len;
+                i++;
+            }
+            else{
+                if(len==0){
+                    lps[i]=0;
+                    i++;
+                }
+                else{
+                    len=lps[len-1];
+                }
+            }
+        }
+        for(int i=0;i<len;i++){
+            ans+=s[i];
+        }
+        return ans;
+    }
+};
