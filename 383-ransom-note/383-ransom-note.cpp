dad1d78919bace3880a607bf65b91064d449c55e
Prateek Praharaj <prateekpj873@gmail.com>
2022-08-25 11:35:28 +0530

    Time: 25 ms (54.61%), Space: 8.8 MB (39.49%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        unordered_map<char,int> mp;
+        vector<int> mp(26,0);
         for(int i=0;i<ransomNote.size();i++){
-            mp[ransomNote[i]]++;
+            mp[ransomNote[i]-'a']++;
         }
         for(int i=0;i<magazine.size();i++){
-            if(mp.find(magazine[i])!=mp.end()){
-                mp[magazine[i]]--;
-                if(mp[magazine[i]]==0){
-                    mp.erase(magazine[i]);
-                }
+            mp[magazine[i]-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(mp[i]>0){
+                return false;
             }
         }
-        return mp.empty();
+        return true;
     }
 };
